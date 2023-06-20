9a56e9dfce16d1ae62a0aea16d0ff7afa968e304
tjcanger <2052717@tongji.edu.cn>
2023-06-20 18:52:08 +0800

    add homepage content

--- src/homepage/index.tsx
+++ src/homepage/index.tsx
@@ -1,5 +1,11 @@
+
 export default function HomePage() {
     return <div>
-        <h1>HomePage</h1>
+        <h1>软件测试课程设计——练习题部分</h1>
+        <br/>
+        <h3>实验室项目——日志解析器关键算法研究</h3>
+        <p>2052717 陈晨</p>
+        <p>2052710 吴笛</p>
+        <p>2052715 段铭健</p>
     </div>
 }
